c84629c10c1ad4a5dd698f925b35739843c97b6e
ducklin404 <duonght404@gmail.com>
2025-05-15 15:40:16 +0700

    FEAT(DB) added mongoose and edit/delete post

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const BlogPosts = require("./blogPosts");
 const bodyParser = require("body-parser");
+const dbConnect = require("./db/dbConnect");
 const jsonParser = bodyParser.json();
-
+const PostRouter = require("./routes/PostRouter");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
+dbConnect();
+app.use(express.json());
+app.use("/api", PostRouter);
+
 
 app.  get("/api/stats", function (req, res) {
   if (req.query.typestat == "count"){
@@ -15,25 +21,6 @@ app.  get("/api/stats", function (req, res) {
   }
 });
 
-app.post("/api/post", jsonParser, (req, res) => {
-  const post = {
-    slug: req.body.slug,
-    title: req.body.title,
-    description: req.body.description,
-  };
-  BlogPosts.BlogPosts.push(post);
-  res.status(200).send({ message: "Posted successful" });
-});
-
-app.get("/api/posts", function (req, res) {
-  res.send(JSON.stringify(BlogPosts.BlogPosts));
-});
-app.get("/api/post/:slug", function (req, res) {
-  const slug = req.params.slug;
-  const post = BlogPosts.BlogPosts.find((element) => element.slug === slug);
-  if (post) res.send(JSON.stringify(post));
-  else res.status(404).send("Not found");
-});
 
 app.post("/api/login", jsonParser, (req, res) => {
   const creds = {
